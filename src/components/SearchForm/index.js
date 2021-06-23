eb722a9eedd20edb5eb1852ea09aecc519bd32cf
Ben F <56423163+bferriman@users.noreply.github.com>
2021-06-22 22:31:29 -0600

    building folder structure and adding starter page and component files

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function SearchForm(props) {
+  return (
+    <div>this is a SearchForm component</div>
+  );
+}
+
+export default SearchForm;
---
+++ src/components/Story/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+function Story(props) {
+  return (
+    <div>this is a Story component</div>
+  );
+}
+
+export default Story;

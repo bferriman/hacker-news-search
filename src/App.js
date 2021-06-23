c173226dd2ff81fa38e4642d6f6e91a122d020ba
Ben F <56423163+bferriman@users.noreply.github.com>
2021-06-23 17:03:21 -0600

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import History from "./pages/History";
+import NavBar from "./components/NavBar";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <BrowserRouter>
+        <NavBar />
+        <Route exact path="/" component={Search}></Route>
+        <Route exact path="/search" component={Search}></Route>
+        <Route exact path="/history" component={History}></Route>
+      </BrowserRouter>
     </div>
   );
 }

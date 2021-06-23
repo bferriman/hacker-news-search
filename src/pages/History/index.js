ca46a02d9cb870660d9a936c7f781158e654d932
Ben F <56423163+bferriman@users.noreply.github.com>
2021-06-23 16:57:13 -0600

    created placeholder content for app's main pages

---
+++ src/pages/History/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function History(props) {
+  return (
+    <h1>this is the History page</h1>
+  );
+}
+
+export default History;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Search(props) {
+  return (
+    <h1>this is the Search page</h1>
+  );
+}
+
+export default Search;

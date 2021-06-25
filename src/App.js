b44d32069480b2ffa09702954745bb48956cd76d
Ben F <56423163+bferriman@users.noreply.github.com>
2021-06-25 14:20:16 -0600

    added redux logic to establish state store

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
 import Search from "./pages/Search";
 import History from "./pages/History";
 import NavTabs from "./components/NavTabs";
 import './App.css';
+import searchesReducer from "./store/reducers/searches";
+
+const store = createStore(searchesReducer);
+
+const SearchPage = () => <Provider store={store}><Search /></Provider>;
+const HistoryPage = () => <Provider store={store}><History /></Provider>;
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <NavTabs />
-        <Route exact path="/" component={Search}></Route>
-        <Route exact path="/search" component={Search}></Route>
-        <Route exact path="/history" component={History}></Route>
+        <Route exact path="/" component={SearchPage}></Route>
+        <Route exact path="/search" component={SearchPage}></Route>
+        <Route exact path="/history" component={HistoryPage}></Route>
       </BrowserRouter>
     </div>
   );
